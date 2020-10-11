82cf19152058394b660aca815a8a1b201b848f41
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-10-11 20:50:45 +0200

    Solved variable bug

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -12,7 +12,7 @@ do
     rsync_docker_destination_path="$docker_backup_folder_path""last/""$docker_container_name$rsync_source_path";
     #backup_dir_path="$backup_dir_base_path$rsync_source_path";
     echo "trying to backup $rsync_source_path..."
-    $rsync_host_destination_path="$HOME$rsync_docker_destination_path";
+    rsync_host_destination_path="$HOME$rsync_docker_destination_path";
     mkdir -p $rsync_host_destination_path;
     docker run --rm --volumes-from "$docker_container_name" -v "$host_backup_folder_path:$docker_backup_folder_path" "kevinveenbirkenbach/alpine-rsync" rsync -a --delete $rsync_source_path $rsync_docker_destination_path || rmdir $rsync_host_destination_path;
   done
