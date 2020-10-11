54db0da552cbeb046723920041f616f7c5f78cfb
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-10-11 19:11:36 +0200

    Added some options

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,7 +11,8 @@ do
   do
     rsync_destination_path="$docker_backup_folder_path""last/""$docker_container_id/$rsync_source_path";
     #backup_dir_path="$backup_dir_base_path$rsync_source_path";
-    docker run --rm --volumes-from "$docker_container_id" -v "$host_backup_folder_path:$docker_backup_folder_path" ubuntu bash -c “test -e $rsync_source_path && rsync -a --delete $rsync_source_path $rsync_destination_path”
+    echo "trying to backup $rsync_source_path..."
+    docker run --rm --volumes-from "$docker_container_id" -v "$host_backup_folder_path:$docker_backup_folder_path" ubuntu bash -c "test -e $rsync_source_path && rsync -a --delete $rsync_source_path $rsync_destination_path"
   done
   echo "start container: $docker_container_id" && docker start "$docker_container_id"
 done
