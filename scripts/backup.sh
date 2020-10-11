e063a05f91f3d021e8ae8c35d2442502c213b8ee
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-10-11 20:19:14 +0200

    Changed image

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -12,7 +12,7 @@ do
     rsync_destination_path="$docker_backup_folder_path""last/""$docker_container_id/$rsync_source_path";
     #backup_dir_path="$backup_dir_base_path$rsync_source_path";
     echo "trying to backup $rsync_source_path..."
-    docker run --rm --volumes-from "$docker_container_id" -v "$host_backup_folder_path:$docker_backup_folder_path" ubuntu bash -c "test -e $rsync_source_path && rsync -a --delete $rsync_source_path $rsync_destination_path"
+    docker run --rm --volumes-from "$docker_container_id" -v "$host_backup_folder_path:$docker_backup_folder_path" "kevinveenbirkenbach/alpine-rsync" bash -c "test -e $rsync_source_path && rsync -a --delete $rsync_source_path $rsync_destination_path"
   done
   echo "start container: $docker_container_id" && docker start "$docker_container_id"
 done
