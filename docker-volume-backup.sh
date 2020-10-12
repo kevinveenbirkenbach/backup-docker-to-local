be2596af98411248700b0bebc10ee903c065f2e3
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-10-12 15:07:37 +0200

    Adapted path

--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# @param $1 [optional] : The path in which the backups folder will be created.
+# @param $1 [optional] : The path for the backups folder. DEFAULT $HOME/Backups/
+
 backup_time="$(date '+%Y%m%d%H%M%S')"
-docker_backups_mount="/Backups/"
+docker_backups_mount="$(test -z "$1" && echo "/Backups/" || echo "/$(basename "$1")/")"
 native_backups_mount_prefix="$(test -z "$1" && echo "$HOME" || echo "$1")"
 native_backups_mount="$native_backups_mount_prefix$docker_backups_mount"
 for docker_container_name in $(docker ps --format '{{.Names}}');
