3eef4cf70a5ef1f4bda4e1b5146db93c910faec8
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-10-12 13:30:02 +0200

    Optimized destination path

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,29 +9,19 @@ do
   echo "stop container: $docker_container_name" && docker stop "$docker_container_name"
   for source_path in $(docker inspect --format '{{ range .Mounts }}{{ if eq .Type "volume" }}{{ println .Destination }}{{ end }}{{ end }}' "$docker_container_name");
   do
-    application_path="$docker_backups_mount$(cat /etc/machine-id)/docker/$docker_container_name/"
-    first_destination_path="$application_path""first""$source_path";
-    latest_destination_path="$application_path""latest""$source_path";
-    backup_dir_path="$application_path""diffs/$backup_time$source_path"
-    if [ -d "$native_backups_mount_prefix$first_destination_path" ]
+    application_path="$docker_backups_mount$(cat /etc/machine-id)/docker/$docker_container_name/";
+    destination_path="$application_path""latest""$source_path";
+    backup_dir_path="$application_path""diffs/$backup_time$source_path";
+    if [ -d "$native_backups_mount_prefix$destination_path" ]
       then
         echo "backup: $source_path"
-        destination_path="$latest_destination_path";
-
       else
         echo "first backup: $source_path"
-        destination_path="$first_destination_path";
-        mkdir -vp "$native_backups_mount_prefix$first_destination_path";
-        mkdir -vp "$native_backups_mount_prefix$latest_destination_path";
+        mkdir -vp "$native_backups_mount_prefix$destination_path";
         mkdir -vp "$native_backups_mount_prefix$backup_dir_path";
     fi
     docker run --rm --volumes-from "$docker_container_name" -v "$native_backups_mount:$docker_backups_mount" "kevinveenbirkenbach/alpine-rsync" sh -c "
-    rsync -ab --delete --delete-excluded --backup-dir=\"$(dirname $backup_dir_path)\" $source_path $destination_path";
-    if [ "$destination_path" == "$first_destination_path" ]
-      then
-        echo "Cloning first to latest..."
-        rsync -a "$native_backups_mount_prefix$first_destination_path" "$native_backups_mount_prefix$latest_destination_path"
-    fi
+    rsync -ab --delete --delete-excluded --backup-dir=\"$(dirname "$backup_dir_path")\" $source_path $destination_path";
   done
   echo "start container: $docker_container_name" && docker start "$docker_container_name"
 done
