660c847383fe00d93d4ee1cb1fe241b01bb931cb
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2021-08-19 13:44:24 +0200

    changed docker_container_name to container_name

--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -7,17 +7,17 @@ backup_time="$(date '+%Y%m%d%H%M%S')";
 backups_folder="/Backups/";
 for volume_name in $(docker volume ls --format '{{.Name}}');
 do
-  for docker_container_name in $(docker ps -a --filter volume=$volume_name --format '{{.Names}}');
+  for container_name in $(docker ps -a --filter volume=$volume_name --format '{{.Names}}');
   do
-    echo "stop container: $docker_container_name" && docker stop "$docker_container_name"
-    for source_path in $(docker inspect --format '{{ range .Mounts }}{{ if eq .Type "volume" }}{{ println .Destination }}{{ end }}{{ end }}' "$docker_container_name");
+    echo "stop container: $container_name" && docker stop "$container_name"
+    for source_path in $(docker inspect --format '{{ range .Mounts }}{{ if eq .Type "volume" }}{{ println .Destination }}{{ end }}{{ end }}' "$container_name");
     do
       repository_name="$(cd "$(dirname "$(readlink -f "${0}")")" && basename `git rev-parse --show-toplevel`)";
       machine_id="$(sha256sum /etc/machine-id | head -c 64)";
       backup_repository_folder="$backups_folder$machine_id/$repository_name/";
-      destination_path="$backup_repository_folder""latest/$docker_container_name$source_path";
+      destination_path="$backup_repository_folder""latest/$container_name$source_path";
       log_path="$backup_repository_folder""log.txt";
-      backup_dir_path="$backup_repository_folder""diffs/$backup_time/$docker_container_name$source_path";
+      backup_dir_path="$backup_repository_folder""diffs/$backup_time/$container_name$source_path";
       if [ -d "$destination_path" ]
         then
           echo "backup: $source_path";
@@ -26,9 +26,9 @@ do
           mkdir -vp "$destination_path";
           mkdir -vp "$backup_dir_path";
       fi
-      docker run --rm --volumes-from "$docker_container_name" -v "$backups_folder:$backups_folder" "kevinveenbirkenbach/alpine-rsync" sh -c "
+      docker run --rm --volumes-from "$container_name" -v "$backups_folder:$backups_folder" "kevinveenbirkenbach/alpine-rsync" sh -c "
       rsync -abP --delete --delete-excluded --log-file=$log_path --backup-dir=$backup_dir_path '$source_path/' $destination_path";
     done
-    echo "start container: $docker_container_name" && docker start "$docker_container_name";
+    echo "start container: $container_name" && docker start "$container_name";
   done
 done
