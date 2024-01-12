d2b699c2716c79af8874a9b3def3fe84b03d2793
Kevin Veen-Birkenbach <kevin@veen.world>
2024-01-12 11:47:46 +0100

    Implemented postgres recovery

--- recover-docker-from-local.sh
+++ recover-docker-from-local.sh
@@ -25,18 +25,31 @@ backup_sql="/$backup_folder/sql/$database_name.backup.sql"
 # DATABASE RECOVERY
 
 if [ -f "$backup_sql" ]; then
-  if [ -n "$database_container" ] && [ -n "$database_password" ] && [ -n "$database_name" ]; then
-    echo "recover mysql dump"
-    cat "$backup_sql" | docker exec -i "$database_container" mariadb -u "$database_user" --password="$database_password" "$database_name"
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Failed to recover mysql dump"
-        exit 1
+  if [ "$database_type" = "postgres" ]; then
+    if [ -n "$database_container" ] && [ -n "$database_password" ] && [ -n "$database_name" ]; then
+      echo "Recover PostgreSQL dump"
+      export PGPASSWORD="$database_password"
+      cat "$backup_sql" | docker exec -i "$database_container" psql -U "$database_user" -d "$database_name"
+      if [ $? -ne 0 ]; then
+          echo "ERROR: Failed to recover PostgreSQL dump"
+          exit 1
+      fi
+      exit 0
     fi
-    exit 0
+  elif [ "$database_type" = "mariadb" ]; then
+    if [ -n "$database_container" ] && [ -n "$database_password" ] && [ -n "$database_name" ]; then
+      echo "recover mysql dump"
+      cat "$backup_sql" | docker exec -i "$database_container" mariadb -u "$database_user" --password="$database_password" "$database_name"
+      if [ $? -ne 0 ]; then
+          echo "ERROR: Failed to recover mysql dump"
+          exit 1
+      fi
+      exit 0
+    fi
   fi
   echo "A database backup exists, but a parameter is missing."
   exit 1
-fi 
+fi
 
 # FILE RECOVERY
 
